cfcfd03be07f9dfe5619c5466f48dac99e6993de
logistic-tech <techpmssa@gmail.com>
2026-08-17 12:46:10 -0500

    cambios

--- controller/serviciosttController.js
+++ controller/serviciosttController.js
@@ -1162,7 +1162,7 @@ export const eliminarServicio =
             "Servicio eliminado correctamente.",
         });
 
-    } catch (error) {
+    } catch (error) { 
       console.error(
         "Error en eliminarServicio:",
         error
