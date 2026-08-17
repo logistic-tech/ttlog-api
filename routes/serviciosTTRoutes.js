8d1fad43fdd0754ff15527d115e5f446efb764a2
logistic-tech <techpmssa@gmail.com>
2026-08-17 14:30:11 -0500

    ca

--- routes/serviciosTTRoutes.js
+++ routes/serviciosTTRoutes.js
@@ -8,7 +8,7 @@ import {
   crearServicio,
   actualizarServicio,
   eliminarServicio,
-} from "../controller/serviciosTTController.js"; 
+} from "../Controllers/serviciosController.js"; 
 
 
 const router =
